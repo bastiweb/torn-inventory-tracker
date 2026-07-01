6604b756a4ddf522f1ca3e606dab50010b3267f5
Sebastian <sebastiangoehler@outlook.com>
2026-07-01 12:29:45 +0200

    initial commit

---
+++ inventory_checker.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import pandas as pd
+from pandas import json_normalize
+
+def get_price_lookup():
+    url = "https://weav3r.dev/api/pricelist/4253363"
+    price_response = requests.get(url)
+    price_data = price_response.json()
+    price_lookup = {}
+
+    for item in price_data:
+        item_id = item["itemId"]
+        price = item["buyPrice"]
+
+        price_lookup[item_id] = price
+
+    return price_lookup
+
+def inventory(api_key):
+    categorie = [
+    "Flower",
+    "Plushie",
+    ]
+
+    price_lookup = get_price_lookup()
+
+    inventory_tables = {}
+
+    def get_inventory_data(categorie):
+        url = f"https://api.torn.com/v2/user/inventory?cat={categorie}&offset=0&limit=20&key={api_key}"
+        response = requests.get(url)
+        return response    
+
+    for categorie in categorie:
+        response = get_inventory_data(categorie)
+
+        data = response.json()
+        if "inventory" not in data:
+            print(data)
+            return {}
+        
+        categorie_json_inventory = json_normalize(data['inventory'])
+        
+        categorie_json_items = json_normalize(categorie_json_inventory['items'].explode())
+
+        categorie_json_items["price"] = categorie_json_items["id"].map(price_lookup)
+        categorie_json_items["amount"] = pd.to_numeric(categorie_json_items["amount"], errors='coerce').fillna(0)
+        categorie_json_items["price"] = pd.to_numeric(categorie_json_items["price"], errors='coerce').fillna(0)
+        categorie_json_items["total_value"] = categorie_json_items["amount"] * categorie_json_items["price"]
+
+        categorie_json_items = categorie_json_items.drop(columns=['id', 'equipped', 'uid', 'faction_owned'], errors='ignore')
+        
+        inventory_tables[categorie] = categorie_json_items
+
+    return inventory_tables
+        #print(categorie_json_items.head())
---
+++ sneaky_steve.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+import tabulate
+import json
+import os
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+from inventory_checker import inventory
+
+load_dotenv()
+KEY_FILE = "api_keys.json"
+
+def get_cipher():
+    secret = os.environ["INVENTORY_BOT_SECRET"]
+    return Fernet(secret.encode())
+
+def encrypt_api_key(api_key):
+    cipher = get_cipher()
+    encrypted_key = cipher.encrypt(api_key.encode())
+    return encrypted_key.decode()
+
+def decrypt_api_key(encrypted_key):
+    cipher = get_cipher()
+    api_key = cipher.decrypt(encrypted_key.encode())
+    return api_key.decode()
+
+def load_keys():
+    if not os.path.exists(KEY_FILE):
+        return {}
+    
+    with open(KEY_FILE, "r") as file:
+        return json.load(file)
+    
+def save_keys(keys):
+    with open(KEY_FILE, "w") as file:
+        json.dump(keys, file, indent=4)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
+#intents.message_content = True
+
+class ApiKeyModal(discord.ui.Modal, title="Enter your API Key"):
+    api_key = discord.ui.TextInput(label="API Key", placeholder="Enter your Torn API Key here")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        keys = load_keys()
+        keys[str(interaction.user.id)] = encrypt_api_key(
+            str(self.api_key.value).strip()
+        )
+        save_keys(keys)
+        await interaction.response.send_message("API key saved successfully!", ephemeral=True)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'We have logged in as {bot.user}')
+
+@bot.tree.command(name="setkey", description="Set your Torn API key")
+async def setkey(interaction: discord.Interaction):
+    await interaction.response.send_modal(ApiKeyModal())
+
+
+@bot.tree.command(name="inventory", description="Check your Torn inventory")
+async def inventory_command(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral=True)
+
+    keys = load_keys()
+    user_id = str(interaction.user.id)
+
+    if user_id not in keys:
+        await interaction.followup.send("API key not found for your user. Please provide your API key using the `/setkey` command.", ephemeral=True)
+        return
+    
+    api_key = decrypt_api_key(keys[user_id])
+
+    inventories = inventory(api_key)
+    messages = []
+
+    for categorie, dataframe in inventories.items():
+        table = tabulate.tabulate(
+            dataframe, 
+            headers='keys', 
+            tablefmt='grid', 
+            showindex=False, 
+            intfmt=',',
+            floatfmt=',.0f'
+        )
+        messages.append(f"```{categorie} Inventory:\n{table}```")
+        
+    await interaction.followup.send("\n".join(messages))
+
+bot.run(os.environ["DISCORD_BOT_TOKEN"])
+
